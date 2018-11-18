f3d29a20f5d59e5640e9862e1e7e6f9fe09f124c
Jennifer Lai <jennifernlai@cs.ucsb.edu>
2018-11-18 14:20:26 -0800

    Added clientid and clientsecret to recsongs.java

--- my-app/src/main/java/com/RecSongs.java
+++ my-app/src/main/java/com/RecSongs.java
@@ -7,10 +7,45 @@ package com;
 public class RecSongs
 {
 
-    SpotifyApi spotifyApi = new SpotifyApi.Builder()
-        .setAccessToken("taHZ2SdB-bPA3FsK3D7ZN5npZS47cMy-IEySVEGttOhXmqaVAIo0ESvTCLjLBifhHOHOIuhFUKPW1WMDP7w6dj3MAZdWT8CLI2MkZaXbYLTeoDvXesf2eeiLYPBGdx8tIwQJKgV8XdnzH_DONk")
-        .build();
+    private static final String clientId = "a1836f665e904cb4869901a56eb1582b";
+    private static final String clientSecret = "cfb9be2440aa427ca22fa07d583833d7";
 
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+          .setClientId(clientId)
+          .setClientSecret(clientSecret)
+          .build();
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+          .build();
+
+    public static void clientCredentials_Sync() {
+        try {
+        final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+        // Set access token for further "spotifyApi" object usage
+        spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+        System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException e) {
+        System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void clientCredentials_Async() {
+        try {
+        final Future<ClientCredentials> clientCredentialsFuture = clientCredentialsRequest.executeAsync();
+
+        // ...
+
+        final ClientCredentials clientCredentials = clientCredentialsFuture.get();
+
+        // Set access token for further "spotifyApi" object usage
+        spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+        System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (InterruptedException | ExecutionException e) {
+        System.out.println("Error: " + e.getCause().getMessage());
+        }
+    }
     RecSongs()
     {
 
